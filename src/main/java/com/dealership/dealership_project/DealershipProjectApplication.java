a6bc4b129fcc6870fd0a2120afb15fba00c49f90
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-05-22 15:20:42 -0300

    Fist commit - getting started

---
+++ src/main/java/com/dealership/dealership_project/DealershipProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dealership.dealership_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealershipProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealershipProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dealership/dealership_project/DealershipProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealership.dealership_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealershipProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
