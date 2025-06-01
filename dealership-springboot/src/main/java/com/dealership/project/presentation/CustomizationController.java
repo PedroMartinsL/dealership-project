993d08fad64849c9ab2cb088da115ba6408b68b3
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-06-01 17:03:53 -0300

    Customization Controller, imports and finishing

--- dealership-springboot/src/main/java/com/dealership/project/presentation/CustomizationController.java
+++ dealership-springboot/src/main/java/com/dealership/project/presentation/CustomizationController.java
@@ -6,28 +6,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.dealership.project.api.dto.OrderDTO;
-import com.dealership.project.application.useCases.car.findAll.ListCarsUseCase;
-import com.dealership.project.application.useCases.car.findAll.ListCarsUseCaseResponse;
-import com.dealership.project.application.useCases.car.findById.GetCarUseCase;
-import com.dealership.project.application.useCases.car.findById.GetCarUseCaseRequest;
-import com.dealership.project.application.useCases.car.findById.GetCarUseCaseResponse;
-import com.dealership.project.application.useCases.order.delete.DeleteOrderUseCaseRequest;
-import com.dealership.project.application.useCases.order.send.SendOrderUseCaseRequest;
-import com.dealership.project.application.useCases.order.send.SendOrderUseCaseResponse;
-import com.dealership.project.application.useCases.order.update.UpdateOrderUseCaseRequest;
-import com.dealership.project.application.useCases.order.update.UpdateOrderUseCaseResponse;
+import com.dealership.project.api.dto.CustomizationDTO;
+import com.dealership.project.application.useCases.customization.findAll.FindAllCustomizationsUseCase;
+import com.dealership.project.application.useCases.customization.findAll.FindAllCustomizationsUseCaseResponse;
+import com.dealership.project.application.useCases.customization.findById.FindByIdCustomizationUseCase;
+import com.dealership.project.application.useCases.customization.findById.FindByIdCustomizationUseCaseRequest;
+import com.dealership.project.application.useCases.customization.findById.FindByIdCustomizationUseCaseResponse;
+import com.dealership.project.application.useCases.customization.send.SendCustomizationUseCase;
+import com.dealership.project.application.useCases.customization.send.SendCustomizationUseCaseRequest;
+import com.dealership.project.application.useCases.customization.send.SendCustomizationUseCaseResponse;
 import com.dealership.project.domain.entities.Customization;
-import com.dealership.project.domain.entities.Order;
 
 @Controller
 @RequestMapping(value = "/customizations")
@@ -45,26 +40,26 @@ public class CustomizationController {
     @GetMapping
     public ResponseEntity<List<Customization>> findAll() {
 		FindAllCustomizationsUseCaseResponse response = findAllCustomizationsUseCase.execute();
-        List<Customization> CustomizationsList = response.Customizations();
-		return ResponseEntity.ok().body(CustomizationsList);
+        List<Customization> customizationsList = response.customizations();
+		return ResponseEntity.ok().body(customizationsList);
 	}
 
     @GetMapping("/{id}")
     public ResponseEntity<Customization> findById(@PathVariable Long id) {
-		FindByIdCustomizationUseCaseRequest CustomizationRequest = new FindByIdCustomizationUseCaseRequest(id);
-        FindByIdCustomizationUseCaseResponse CustomizationReponse = findByIdCustomizationUseCase.execute(CustomizationRequest);
-        Customization Customization = CustomizationReponse.Customization();
-		return ResponseEntity.ok().body(Customization);
+		FindByIdCustomizationUseCaseRequest customizationRequest = new FindByIdCustomizationUseCaseRequest(id);
+        FindByIdCustomizationUseCaseResponse customizationReponse = findByIdCustomizationUseCase.execute(customizationRequest);
+        Customization customization = customizationReponse.customization();
+		return ResponseEntity.ok().body(customization);
 	}
 
     @PostMapping
     public ResponseEntity<Customization> insert(
-        @RequestBody CustomizationDTO CustomizationDTO
+        @RequestBody CustomizationDTO customizationDTO
     ) {
-        SendCustomizationUseCaseRequest CustomizationRequest = new SendCustomizationUseCaseRequest(CustomizationDTO);
-        SendCustomizationUseCaseResponse CustomizationResponse = sendCustomizationUseCase.execute(CustomizationRequest);
-        Customization Customization = CustomizationResponse.Customization();
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(Customization.getId()).toUri();
-		return ResponseEntity.created(uri).body(Customization);
+        SendCustomizationUseCaseRequest customizationRequest = new SendCustomizationUseCaseRequest(customizationDTO);
+        SendCustomizationUseCaseResponse customizationResponse = sendCustomizationUseCase.execute(customizationRequest);
+        Customization customization = customizationResponse.customization();
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(customization.getId()).toUri();
+		return ResponseEntity.created(uri).body(customization);
     }
 }
