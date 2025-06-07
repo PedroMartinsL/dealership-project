dfdaa97b1c58a9d6af1dcff868c76392efe4d54b
Gabriel Lopes <gabriel.lopes.albuquerque@gmail.com>
2025-06-07 09:46:15 -0300

    feat: adiciona rotas e páginas iniciais (Home e Modelos)

---
+++ dealership-vue/src/components/Highlights.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="highlight-section">
+      
+    <!-- HighLight 2 -->
+     
+      <div
+        class="highlight"
+        :style="{ backgroundImage: `url(${mercedesBanner})` }"
+      >
+        <div class="highlight-content">
+          <h2>Mercedes-Benz GLB.</h2>
+          <p>Design moderno com 7 lugares. Pronta entrega.</p>
+          <button>Saiba mais</button>
+        </div>
+      </div>
+
+      <div class="highlight-separator"></div>
+  
+      <!-- HighLight 2 -->
+      <div
+        class="highlight"
+        :style="{ backgroundImage: `url(${rollBanner})` }"
+      >
+        <div class="highlight-content">
+          <h2>Rolls-Royce Phantom.</h2>
+          <p>Exclusividade, luxo e potência à sua altura.</p>
+          <button>Saiba mais</button>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script setup lang="ts">
+  import mercedesBanner from '../assets/mercedes-banner.jpeg'
+  import rollBanner from '../assets/roll-banner.jpg'
+  </script>
+  
---
+++ dealership-vue/src/pages/Modelos.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="modelos-page">
+      <h1>Modelos e Preços</h1>
+  
+      <div class="modelos-grid">
+        <div class="modelo-card">
+          <img src="../assets/audir8.webp" alt="AUDI R8" />
+          <h2>BMW iX</h2>
+          <span class="categoria">Elétrico</span>
+          <p>A partir de R$ 726.950 à vista*</p>
+        </div>
+  
+        <div class="modelo-card">
+          <img src="../assets/landrover.png" alt="LAND ROVER" />
+          <h2>BMW i7</h2>
+          <span class="categoria">Elétrico</span>
+          <p>A partir de R$ 1.372.950 à vista*</p>
+        </div>
+  
+        <div class="modelo-card">
+          <img src="../assets/bmwix.webp" alt="BMW IX" />
+          <h2>BMW i4 Gran Coupé</h2>
+          <span class="categoria">Elétrico</span>
+          <p>A partir de R$ 581.950 à vista*</p>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script setup lang="ts"></script>
+  
